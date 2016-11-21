c85feba05e3ed65c71b6329110d8d35e516ae09e
Martina <martina.tosevska.95@gmail.com>
2016-11-21 16:01:27 +0100

    Basic navigation

--- app/src/main/java/das/songrecorder/HomeActivity.java
+++ app/src/main/java/das/songrecorder/HomeActivity.java
@@ -1,7 +1,10 @@
 package das.songrecorder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +12,28 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Button startButton=(Button)this.findViewById(R.id.start);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), RecordActivity.class));
+            }
+        });
+
+        Button viewFiles=(Button)this.findViewById(R.id.view);
+        viewFiles.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        Button exit=(Button)this.findViewById(R.id.exit);
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
--- app/src/main/java/das/songrecorder/RecordActivity.java
+++ app/src/main/java/das/songrecorder/RecordActivity.java
@@ -2,6 +2,8 @@ package das.songrecorder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RecordActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class RecordActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record);
+        Button stop_pause=(Button)this.findViewById(R.id.stop_pause);
+        stop_pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
