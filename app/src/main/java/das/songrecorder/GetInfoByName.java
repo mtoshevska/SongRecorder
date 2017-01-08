c2e9296be2f5ebd4b68056d4ec834f1aea49fecd
Martina <martina.tosevska.95@gmail.com>
2017-01-09 00:44:21 +0100

    Database connection

--- app/src/main/java/das/songrecorder/GetInfoByName.java
+++ app/src/main/java/das/songrecorder/GetInfoByName.java
@@ -7,6 +7,10 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.Random;
 
 /**
@@ -67,6 +71,56 @@ public class GetInfoByName implements GetInfo {
             song.setArtist("Artist"+num);
             song.setGenre("Genre"+num);
             song.setYear(2017);
+
+            String connectionUrl="jdbc:jtds:sqlserver://songrecorder.database.windows.net:1433/Songs;user=dasproject@songrecorder;password=power!Tgirls;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
+
+            // Declare the JDBC objects.
+            Connection con = null;
+            PreparedStatement stmt = null;
+            ResultSet rs = null;
+            try
+            {
+                // Establish the connection.
+                Class.forName("net.sourceforge.jtds.jdbc.Driver").newInstance();
+                con = DriverManager.getConnection(connectionUrl);
+                Log.d("GetInfoByName","connected");
+
+                // Create and execute an SQL statement that returns some data.
+                stmt = con.prepareStatement("SELECT * FROM SongsInfo WHERE Name='"+params[0]+"'");
+                Log.d("GetInfoByName","statement created");
+                rs = stmt.executeQuery();
+                Log.d("GetInfoByName","statement executed");
+
+                // Iterate through the data in the result set and display it.
+                while (rs.next()) {
+                    Log.d("GetInfoByName",rs.getString("Name")+" "+rs.getString("Author")+" "+rs.getString("Artist")+" "+rs.getString("Genre")+""+rs.getInt("Year"));
+                    song.setAuthor(rs.getString("Author"));
+                    song.setArtist(rs.getString("Artist"));
+                    song.setGenre(rs.getString("Genre"));
+                    song.setYear(rs.getInt("Year"));
+                }
+            }
+            // Handle any errors that may have occurred.
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+            finally
+            {
+                if (rs != null) try {
+                    rs.close();
+                } catch (Exception e) {
+                }
+                if (stmt != null) try {
+                    stmt.close();
+                } catch (Exception e) {
+                }
+                if (con != null) try {
+                    con.close();
+                } catch (Exception e) {
+                }
+            }
+
             Intent intent = new Intent();
             intent.setAction("SongFilledWithInformation");
             intent.putExtra("Song",song);
